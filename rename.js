d2b497a43b11bc2399226c4cbfc31449f47e5584
Kitt <kittphi@gmail.com>
2021-11-30 11:13:36 -0700

    working

--- rename.js
+++ rename.js
@@ -1,39 +1,38 @@
 ﻿
 var fs = require('fs');
 
+// All files in files directory
 var files = fs.readdirSync('./files');
 
+// Loop thru each file in files directory
 files.forEach(function (file) {
-    var currentname = './files/' + file, 
-        date = parseDate(file),
-        newname = './files/' + date.split(';').join('') + '.mp3';
+    var currentname = './files/' + file, // // Mon Jan 03 10;05;47 2011.mp3
+        date = parseDate(file), // 10;05;47
+        newname = './files/' + date.split(';').join('') + '.mp3'; // renames 10;05;47 to 100547
 
     renameFile(currentname, newname); 
     console.log('Renamed ' + currentname + ' to ' + newname);    
 }); 
 
-
 /*
     This function renames a file
-    
+
     @param: (String) currentname - current file name
     @param: (String) newname - new file name     
 */
 function renameFile(currentname, newname) {
     fs.renameSync(currentname, newname);
-}
-
+};
 
 /*
-    Extracts date/time from file name string
+    Extracts time E.g. 10;05;47 from file name
     
     @param: (String) filename - file name 
 */
 function parseDate(filename) {
-    var date = filename.substring(11, 19);
-    
-    return date;
-}
+    var date = filename.substring(11, 19); // Mon Jan 03 10;05;47 2011.mp3 (11 is before, 19 is last number)
+    return date;  // 10;05;47
+};
 
 console.log('Done');
 
--- stream.js
+++ stream.js
@@ -4,36 +4,38 @@ var fs = require('fs'),
     clips = [],
     stream,
     currentfile,
-    dhh = fs.createWriteStream('./dhh-interview.mp3');
+    joined = fs.createWriteStream('./joined-interview.mp3');
 
 // create an array with filenames (time)
+// file - must be in format 100547.mp3
 files.forEach(function (file) {
-    clips.push(file.substring(0, 6));  
+    clips.push(file.substring(0, 6));  // 100547.mp3 (first to last)
 });
 
-// Sort
+// Sort it by time (hour, minute, seconds)
 clips.sort(function (a, b) {
     return a - b;
 });
 
-
 // recursive function
 function main() {
+    // if no files are left in array, then end
     if (!clips.length) {
-        dhh.end("Done");
+        joined.end("Done");
         return;
-    }
+    };
     
+    // pop off first file in array and save it as currentfile
     currentfile = './files/' + clips.shift() + '.mp3';
+    // Read that file and write it to joined-interview.mp3
     stream = fs.createReadStream(currentfile);
-    
-    stream.pipe(dhh, {end: false});
-    
+    // do not end
+    stream.pipe(joined, {end: false});
+    // listen on end, if end is false then log. Run the program again
     stream.on("end", function() {
         console.log(currentfile + ' appended');
         main();        
     });
-}
-
+};
 
 main();
